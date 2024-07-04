ae5ff976949659f6d91bd3b5dbce611798bf160d
anyaaseth08 <anyaaseth@gmail.com>
2024-07-05 00:35:02 +0530

    New Files Added

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -22,3 +22,51 @@ public class LargestElement{
     
     }
 }
+
+/*
+import java.util.*;
+public class LargestElement{
+    public static void largestElement(int[] arr){
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = i + 1 ; j < arr.length ; j++){
+                  if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                  }
+            }
+        } 
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the size of Array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the Array : ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        largestElement(arr);
+        System.out.println("Largest element in the Array is : " + arr[n-1]);
+    }
+}
+
+import java.util.*;
+public class LargestElement{
+    public static void largestElement(int[] arr){
+        Arrays.sort(arr);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the size of Array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the Array : ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        largestElement(arr);
+        System.out.println("Largest element in the Array is : " + arr[n-1]);
+    }
+}
+*/
