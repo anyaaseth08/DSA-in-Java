1e1a578fd580350fc25f01af0d45e5222b7cc557
anyaaseth08 <anyaaseth@gmail.com>
2024-07-06 00:55:11 +0530

    code added

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -1,4 +1,5 @@
-//Time Complexity : O(N)
+Time Complexity : O(N)
+left rotate array by one place
 import java.util.*;
 public class RotateArray {
     public static void rotateArray(int[] arr) {
@@ -20,6 +21,81 @@ public class RotateArray {
         rotateArray(arr);
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]+" ");
+        }
+    }
+}
+
+left rotate array by D place
+Brute Force Approach
+Time Complexity: O(N+D)
+Space Complexity: O(d)
+
+import java.util.*;
+public class RotateArray {
+    public static void rotateArray(int[] arr, int n, int d) {
+        d = d % n;
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+        for (int i = d; i < n; i++) {
+            arr[i - d] = arr[i];
+        }
+        for (int i = n - d; i < n; i++) {
+            arr[i] = temp[i - (n - d)];
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the Array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the number of times you want to rotate array : ");
+        int d = sc.nextInt();
+        rotateArray(arr, n, d);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+Optimal Approach
+Time Complexity: O(N*D)
+Space Complexity: O(1)
+import java.util.*;
+public class RotateArray {
+    public static void leftRotate(int arr[], int d, int n)
+    {
+        for (int i = 0; i < d; i++)
+            leftRotatebyOne(arr, n);
+    }
+    public static void leftRotatebyOne(int arr[], int n)
+    {
+        int i, temp;
+        temp = arr[0];
+        for (i = 0; i < n - 1; i++)
+            arr[i] = arr[i + 1];
+        arr[i] = temp;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the Array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the number of times you want to rotate array : ");
+        int d = sc.nextInt();
+        leftRotate(arr,d,n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
